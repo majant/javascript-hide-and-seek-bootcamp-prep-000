7f1ed77527156433205f18677a8e6984054dcaba
Antonina Maj <antoninajanina@gmail.com>
2019-02-07 22:10:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target")
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelector("#app").querySelectorAll("ul.ranked-list li")
+  for (let i = 0; i< lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+  return
+}
+
+function deepestChild() {
+  var current = document.querySelector("#grand-node")
+  while (current.children.length !== 0) {
+    current = current.children[0]
+  }
+  return current
+}
